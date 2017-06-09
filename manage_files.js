a663661197588c96f540f75942dae70dd37c0e5c
johanmorales <jhellmetal2000@gmail.com>
2017-06-09 11:30:06 -0500

    Se mejora el reproductor, se añaden las funcionalidades de duración de canción y barra de progreso de la misma, se añade un poco de estilo y organizacion a la vista reproductor

--- manage_files.js
+++ manage_files.js
@@ -2,14 +2,43 @@
 var fs = require("fs")
 var path = require('path')
 //-----------------------------------------------
+
+//-----------------------------------------------------
+//Función para ordenamiento natural de strings con numeros que le preceden
+//https://stackoverflow.com/questions/15478954/sort-array-elements-string-with-numbers-natural-sort
+var chunkRgx = /(_+)|([0-9]+)|([^0-9_]+)/g;
+
+function naturalCompare(a, b) {
+    var ax = [], bx = [];
+    
+    a.replace(chunkRgx, function(_, $1, $2, $3) {
+        ax.push([$1 || "0", $2 || Infinity, $3 || ""])
+    });
+    b.replace(chunkRgx, function(_, $1, $2, $3) {
+        bx.push([$1 || "0", $2 || Infinity, $3 || ""])
+    });
+    
+    while(ax.length && bx.length) {
+        var an = ax.shift();
+        var bn = bx.shift();
+        var nn = an[0].localeCompare(bn[0]) || 
+                 (an[1] - bn[1]) || 
+                 an[2].localeCompare(bn[2]);
+        if(nn) return nn;
+    }
+    
+    return ax.length - bx.length;
+}
+//-----------------------------------------------------
+
 exports.getBiblioteca = function(srcpath){
 	return fs.readdirSync(srcpath).filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory());
 }
 
 exports.getAlbum = function(srcpath){
-	return fs.readdirSync(srcpath).filter(function(item){ var str_pos = item.indexOf(".mp3"); return str_pos === -1 ? false : true });
+	return fs.readdirSync(srcpath).filter(function(item){ var str_pos = item.indexOf(".mp3"); return str_pos === -1 ? false : true }).sort(naturalCompare);
 }
 
 exports.getCover = function(srcpath){
-	return fs.readdirSync(srcpath).filter(function(item){ var str_pos = item.indexOf("Cover.jpg"); return str_pos === -1 ? false : true });
+	return fs.readdirSync(srcpath).filter(function(item){ var str_pos = item.indexOf(".jpg"); return str_pos === -1 ? false : true });
 }
--- server.js
+++ server.js
@@ -46,6 +46,8 @@ function createDiv(contenido){
 	return "<div><a href='/reproductor?nombre_album="+contenido+"'>"+contenido+" <span class='glyphicon glyphicon-chevron-right'></span></a> </div>";
 }
 
+
+
 app.get('/reproductor', function(req,res){
 
 	var nom_album = req.query.nombre_album;
@@ -73,20 +75,18 @@ function createAlbum(lista, path_album, nombre_album){
 }
 
 function createTitulo(nombre, path_album){
-	return "<h3>"+nombre+"</h3> <img src='/public/music/Decapitated-Nihility-2002/"+manage_files.getCover(path_album)+"' height='200' width='200'>";
+	return "<h3>"+nombre+"</h3> <img class='img-responsive img-thumbnail' src='/public/music/"+nombre+"/"+manage_files.getCover(path_album)+"' height='300' width='300'> ";
 }
 
 function createListaCanciones(lista, nombre_album){
 
-	var res = "";
+	var res = "<ul class='text-left lista-canciones'>";
 
 	lista.forEach( function(element, index) {
 		res += createLiSong(element, nombre_album);
-	});
+	});	
 
-	//res += createUl(res);
-
-	return res;
+	return res+"</ul>";
 }
 
 function createUl(cont){
@@ -97,50 +97,6 @@ function createLiSong(nombre_cancion, nombre_album){
 	//<div>'+nombre_cancion+'</div> 
 	return '<li> <a href="#" class="item-lista" data-src="/public/music/'+nombre_album+'/'+nombre_cancion+'" data-name="'+nombre_cancion+'">'+nombre_cancion+'</a></li>';
 }
-
-/*
-<h3>Decapitated - Nihility (2002)</h3>
-<ul>
-	<li>
-		<div>06 - Spheres Of Madness.mp3</div>
-		<audio controls hidden="true">							
-			<source src="music/Decapitated_Nihility/06 - Spheres Of Madness.mp3">				
-		</audio>
-
-	</li>
-	
-	<li></li>
-</ul>
-*/
-
-/*
-Se declaran la peticion que va a hacer que se reproduzca la canción
-*/
-//Decapitated - Nihility (2002)
-/*
-app.get('/music/Decapitated_Nihility', function(llamado, respuesta){
-
-	var id_archivo = llamado.query.id;
-
-	var archivo = __dirname+'/public/music/Decapitated_Nihility/'+id_archivo;
-
-	console.log(archivo)
-
-	fs.exists(archivo, function(existe){
-
-		if (existe) {
-			//crea el stream y la pipe
-			var lee_stream = fs.createReadStream(archivo);
-			lee_stream.pipe(respuesta);
-
-		} else {
-			//de locontrario crea un mensaje de error
-			respuesta.send("El archivo no existe [404].")
-			respuesta.end()
-		}
-	})	
-})*/
-
 
 //-------------------------------------------------------------
 app.listen(3000, function(){
